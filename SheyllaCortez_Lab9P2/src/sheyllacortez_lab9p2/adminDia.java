ca14fdf8b930cf404287d09a372b4e468ea82fc8
unknown <sheylla10>
2024-03-15 13:46:59 -0600

    Clase adminprogress y clase admintiempo

--- SheyllaCortez_Lab9P2/src/sheyllacortez_lab9p2/adminDia.java
+++ SheyllaCortez_Lab9P2/src/sheyllacortez_lab9p2/adminDia.java
@@ -4,10 +4,39 @@
  */
 package sheyllacortez_lab9p2;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JLabel;
+
 /**
  *
  * @author DELL
  */
-public class adminDia {
-    
+public class adminDia implements Runnable {
+
+    private JLabel miHora;
+    private JLabel fecha;
+
+    public adminDia(JLabel miHora, JLabel fecha) {
+        this.miHora = miHora;
+        this.fecha = fecha;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            Date tiempo = new Date();
+            DateFormat Tformato = new SimpleDateFormat("hh:mm:ss");
+            DateFormat Hformato = new SimpleDateFormat("dd/MM/yyyy");
+            miHora.setText(Tformato.format(tiempo));
+            fecha.setText(Hformato.format(tiempo));
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+
+            }
+        }
+    }
+
 }
--- SheyllaCortez_Lab9P2/src/sheyllacortez_lab9p2/adminProgress.java
+++ SheyllaCortez_Lab9P2/src/sheyllacortez_lab9p2/adminProgress.java
@@ -4,10 +4,59 @@
  */
 package sheyllacortez_lab9p2;
 
+import javax.swing.JProgressBar;
+
 /**
  *
  * @author DELL
  */
-public class adminProgress {
-    
+public class adminProgress extends Thread{
+    private JProgressBar prBar;
+    private boolean sigue;
+    private boolean termina;
+
+    public adminProgress(JProgressBar prBar, boolean sigue, boolean termina) {
+        this.prBar = prBar;
+        this.sigue = sigue;
+        this.termina = termina;
+    }
+
+    public JProgressBar getPrBar() {
+        return prBar;
+    }
+
+    public void setPrBar(JProgressBar prBar) {
+        this.prBar = prBar;
+    }
+
+    public boolean isSigue() {
+        return sigue;
+    }
+
+    public void setSigue(boolean sigue) {
+        this.sigue = sigue;
+    }
+
+    public boolean isTermina() {
+        return termina;
+    }
+
+    public void setTermina(boolean termina) {
+        this.termina = termina;
+    }
+
+    @Override
+    public void run() {
+        while (termina) {
+            if (sigue) {
+                prBar.setValue(prBar.getValue() + 1);
+                prBar.setString(Integer.toString(prBar.getValue()) + " mitiempo");
+            }
+            try {
+                Thread.sleep(700);
+            } catch (InterruptedException ex) {
+            }
+        }
+    }
+
 }
